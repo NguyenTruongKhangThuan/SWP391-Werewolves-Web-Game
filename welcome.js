1b6aeb8967cc58d2b1de238af86207396cb120b2
duykhoa1234 <132961706+duykhoa1234@users.noreply.github.com>
2023-06-02 16:23:33 +0700

    welcome.js

--- welcome.js
+++ welcome.js
@@ -1,15 +1,31 @@
 import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
 function Welcome(){
     return(
         <Fragment>
           <h3>Welcome to Werewolves Web Page</h3>
-          <a href="#">About</a>
-          <a href="#">What is Werewolves?</a>
-          <a href="#">Roles</a>
-          <a href="#">FAQs</a>
-          <a href="login.html">
+          {/* <a href="">About</a>
+          <a href="">What is Werewolves?</a>
+          <a href="">Roles</a>
+          <a href="">FAQs</a> */}
+          <Link to=''>
+            <button>About</button>
+        </Link>
+        <Link to=''>
+            <button>What is Werewolf?</button>
+        </Link>
+        <Link to=''>
+            <button>Roles</button>
+        </Link>
+        <Link to=''>
+            <button>FAQs</button>
+        </Link>
+          {/* <a href="login.html">
         <button type="submit">Play</button>
-    </a>
+    </a> */}
+        <Link to='/login'>
+            <button>Play</button>
+        </Link>
         </Fragment>
     );
 }
